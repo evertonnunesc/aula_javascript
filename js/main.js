066dfb7e7b8df2b2f5660b3e8a57ce19b69eca5e
Everton Nunes <evertonnunesc@gmail.com>
2020-08-23 16:55:36 -0300

    aula de javascript basica

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+/*function botao(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
+    //console.log(document.getElementById("agradecimento"))
+    //alert("Obrigado por clicar!");
+}
+
+function redirecionar(){
+    window.open("http://www.google.com.br") ABRE EM JANELA SEPARADA
+    window.location.href = "http://www.google.com.br" ABRE NA MESMA JANELA
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}*/
+
+/* DATA
+var d = new Date();
+alert(d);*/
+
+/* ESTRUTURA DE REPETIÇÃO
+var contar = 0;
+while(contar <= 5 ){ 
+    console.log(contar);
+    contar = contar + 1; ou "contar++" recebe ele e mais 1
+};
+
+var contar;
+for(contar=0; contar < 5; contar++){
+    alert(contar);
+}*/
+
+
+/* FUNÇÃO SE E ENTÃO
+var idade = prompt("Qual é a sua idade?");
+if (idade >= 18) {
+    alert("Maior de idade!");
+} else {
+    alert("Menor de idade!");
+};*/
+
+/* BIBLIOTECA
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta);*/
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva"); ACRESCENTA UMA VARIVEL
+//lista.pop(); TIRA UMA VARIAVEL
+
+//console.log(lista);
+//console.log(lista.toString()); TRANSFORMA A LISTA EM STRNG
+//console.log(lista.join(" - ")); MUDA A VIRGULA PELO CARACTERE
+
+
+//var nome = "Everton Nunes";
+//var idade = 23;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo!"
+//alert(nome + " tem " + idade + " anos ");
+// alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil")); MUDA O NOME DA VARIAVEL
