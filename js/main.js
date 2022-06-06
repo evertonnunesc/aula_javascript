f6d82a150d04711f544a087d0d4dfad62ae0e310
Everton Nunes <evertonnunesc@gmail.com>
2022-06-06 01:56:07 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
-/*function botao(){
+function botao(){
     document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
-    //console.log(document.getElementById("agradecimento"))
-    //alert("Obrigado por clicar!");
+    console.log(document.getElementById("agradecimento"))
+    alert("Obrigado por clicar!");
 }
 
 function redirecionar(){
@@ -11,20 +11,20 @@ function redirecionar(){
 }
 
 function trocar(elemento){
-    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
     elemento.innerHTML = "Obrigado por passar o mouse";
 }
 
 function voltar(elemento){
-    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
     elemento.innerHTML = "Passe o mouse aqui";
-}*/
+}
 
-/* DATA
+ DATA
 var d = new Date();
-alert(d);*/
+alert(d);
 
-/* ESTRUTURA DE REPETIÇÃO
+ ESTRUTURA DE REPETIÇÃO
 var contar = 0;
 while(contar <= 5 ){ 
     console.log(contar);
@@ -34,36 +34,36 @@ while(contar <= 5 ){
 var contar;
 for(contar=0; contar < 5; contar++){
     alert(contar);
-}*/
+}
 
 
-/* FUNÇÃO SE E ENTÃO
+FUNÇÃO SE E ENTÃO
 var idade = prompt("Qual é a sua idade?");
 if (idade >= 18) {
     alert("Maior de idade!");
 } else {
     alert("Menor de idade!");
-};*/
+};
 
-/* BIBLIOTECA
+BIBLIOTECA
 var fruta = {nome:"maçã", cor:"vermelha"};
-console.log(fruta);*/
+console.log(fruta);
 
-//var lista = ["maça", "pera", "laranja"];
-//lista.push("uva"); ACRESCENTA UMA VARIVEL
-//lista.pop(); TIRA UMA VARIAVEL
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva"); ACRESCENTA UMA VARIVEL
+lista.pop(); TIRA UMA VARIAVEL
 
-//console.log(lista);
-//console.log(lista.toString()); TRANSFORMA A LISTA EM STRNG
-//console.log(lista.join(" - ")); MUDA A VIRGULA PELO CARACTERE
+console.log(lista);
+console.log(lista.toString()); TRANSFORMA A LISTA EM STRNG
+console.log(lista.join(" - ")); MUDA A VIRGULA PELO CARACTERE
 
 
-//var nome = "Everton Nunes";
-//var idade = 23;
-//var idade2 = 10;
-//var frase = "Japão é o melhor time do mundo!"
-//alert(nome + " tem " + idade + " anos ");
-// alert(idade + idade2);
-//console.log(nome);
-//console.log(idade + idade2);
-//console.log(frase.replace("Japão", "Brasil")); MUDA O NOME DA VARIAVEL
+var nome = "Everton Nunes";
+var idade = 23;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo!"
+alert(nome + " tem " + idade + " anos ");
+alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil")); MUDA O NOME DA VARIAVEL
